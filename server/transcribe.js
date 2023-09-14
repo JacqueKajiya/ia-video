0a3ae650da9c2e2ac8d30e1120924649ad76b6c5
JacqueKajiya <jacquelinekajiya@gmail.com>
2023-09-14 14:11:17 -0300

    feat: added transcription features

--- server/transcribe.js
+++ server/transcribe.js
@@ -1,5 +1,22 @@
+import { pipeline } from "@xenova/transformers"
 import { transcriptionExample } from "../utils/transcription"
 
-export async function transcribe() {
-  return transcriptionExample
+export async function transcribe(audio) {
+  try {
+    console.log("Realizando a transcrição...")
+
+    const transcribe = await pipeline(
+      "automatic-speech-recognition",
+      "Xenova/whisper-small"
+    )
+
+    const transcription = await transcribe(audio, {
+      chunk_length_s: 30,
+      stride_length_s: 5,
+      language: "portuguese",
+      task: "transcribe",
+    })
+  } catch (error) {
+    throw new Error(error)
+  }
 }
