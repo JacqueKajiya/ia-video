c7ab76302869c746948d2a6bbe5980af85248742
JacqueKajiya <jacquelinekajiya@gmail.com>
2023-09-14 11:02:20 -0300

    feat: added summary to function and edited classes from placeholder

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ import express from "express"
 
 import { download } from "./download.js"
 import { transcribe } from "./transcribe.js"
+import { summarize } from "./summaryze.js"
 
 const app = express()
+app.use(express.json())
 app.use(cors())
 
 app.get("/summary/:id", async (req, res) => {
@@ -12,6 +14,11 @@ app.get("/summary/:id", async (req, res) => {
   const result = await transcribe()
 
   res.json({ result })
+})
+
+app.post("/summary", async (req, res) => {
+  const result = await summarize(req.body.text)
+  return res.json({ result })
 })
 
 app.listen(3333, () => console.log("Server is running on port 3333"))
